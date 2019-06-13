a013be3f0354e0378af4cdd1907daaadb50fdf48
Michael Wolz <michaelwolz93@gmail.com>
2019-06-13 19:20:56 +0200

    Fixed performance issue in MOST CITED function.

--- src/main/java/de/unitrier/dbis/schenql/compiler/visitor/PublicationAggregateFunctionVisitor.java
+++ src/main/java/de/unitrier/dbis/schenql/compiler/visitor/PublicationAggregateFunctionVisitor.java
@@ -9,14 +9,14 @@ public class PublicationAggregateFunctionVisitor extends SchenqlParserBaseVisito
         if (ctx.MOST_CITED() != null) {
             PublicationQueryVisitor pqv = new PublicationQueryVisitor();
             if (!(ctx.getParent().getParent().getRuleContext() instanceof SchenqlParser.QueryContext)) {
-                return "SELECT `sub`.`dblpKey` FROM (" +
+                return "SELECT * FROM (SELECT DISTINCT `sub`.`dblpKey` FROM (" +
                         pqv.visitPublicationQuery(ctx.publicationQuery(), new String[]{
                                 "`publication`.`dblpKey`"
                         }) +
                         ") as `sub` " +
                         "JOIN `publication_references` " +
                         "ON `publication_references`.`pub2_id` = `sub`.`dblpKey` " +
-                        "GROUP BY `sub`.`dblpKey` ORDER BY COUNT(`sub`.`dblpKey`) DESC";
+                        "GROUP BY `sub`.`dblpKey` ORDER BY COUNT(`sub`.`dblpKey`) DESC LIMIT 5) as optim";
             }
 
             return "SELECT `sub`.`title`, `sub`.`year`, COUNT(`sub`.`dblpKey`) as `citations` FROM (" +
