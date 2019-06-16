1f77a525d66320929c988668294405a32d2a6520
Michael Wolz <michaelwolz93@gmail.com>
2019-06-16 15:43:38 +0200

    Fixed CONFERENCE | JOURNAL OF

--- src/main/java/de/unitrier/dbis/schenql/compiler/visitor/PublicationVisitor.java
+++ src/main/java/de/unitrier/dbis/schenql/compiler/visitor/PublicationVisitor.java
@@ -9,15 +9,16 @@ public class PublicationVisitor extends SchenqlParserBaseVisitor<String> {
     public String visitPublication(SchenqlParser.PublicationContext ctx) {
         if (ctx.publicationQuery() != null) {
             PublicationQueryVisitor pqv = new PublicationQueryVisitor();
+            System.out.println("Hallo");
             if (ctx.getParent().getRuleContext() instanceof SchenqlParser.PublicationLimitationContext
                     || ctx.getParent().getRuleContext() instanceof SchenqlParser.PersonLimitationContext) {
                 return pqv.visitPublicationQuery(ctx.publicationQuery(), new String[]{"`publication`.`dblpKey`"});
             }
             if (ctx.getParent().getRuleContext() instanceof SchenqlParser.JournalLimitationContext) {
-                return pqv.visitPublicationQuery(ctx.publicationQuery(),  new String[]{"`publication`.`journal_dblpKey`"});
+                return pqv.visitPublicationQuery(ctx.publicationQuery(), new String[]{"`publication`.`journal_dblpKey`"});
             }
             if (ctx.getParent().getRuleContext() instanceof SchenqlParser.ConferenceLimitationContext) {
-                return pqv.visitPublicationQuery(ctx.publicationQuery(),  new String[]{"`publication`.`conference_dblpKey`"});
+                return pqv.visitPublicationQuery(ctx.publicationQuery(), new String[]{"`publication`.`conference_dblpKey`"});
             }
             return pqv.visitPublicationQuery(ctx.publicationQuery());
         } else if (ctx.DBLP_KEY() != null) {
@@ -25,6 +26,15 @@ public class PublicationVisitor extends SchenqlParserBaseVisitor<String> {
         } else {
 //            return "SELECT `publication`.`dblpKey` FROM `publication` WHERE " +
 //                    "MATCH(`publication`.`title`) AGAINST(\"" + ctx.STRING().getText() + "\")";
+            // TODO: This is not nice
+            if (ctx.getParent().getRuleContext() instanceof SchenqlParser.JournalLimitationContext) {
+                return "SELECT `publication`.`journal_dblpKey` FROM `publication` WHERE " +
+                        "`publication`.`title` " + Helper.sqlStringComparison(ctx.STRING().getText());
+            }
+            if (ctx.getParent().getRuleContext() instanceof SchenqlParser.ConferenceLimitationContext) {
+                return "SELECT `publication`.`conference_dblpKey` FROM `publication` WHERE " +
+                        "`publication`.`title` " + Helper.sqlStringComparison(ctx.STRING().getText());
+            }
             return "SELECT `publication`.`dblpKey` FROM `publication` WHERE " +
                     "`publication`.`title` " + Helper.sqlStringComparison(ctx.STRING().getText());
         }
