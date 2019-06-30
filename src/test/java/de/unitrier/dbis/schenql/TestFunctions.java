9d95f665f32bda9c2d89a6558560384768dd9479
Michael Wolz <michaelwolz93@gmail.com>
2019-06-30 17:19:40 +0200

    Updated tests

--- src/test/java/de/unitrier/dbis/schenql/TestFunctions.java
+++ src/test/java/de/unitrier/dbis/schenql/TestFunctions.java
@@ -24,7 +24,7 @@ public class TestFunctions {
         String schenQLQuery1 = "MOST CITED (PUBLICATIONS)";
         String schenQLQuery2 = "PERSONS AUTHORED (MOST CITED (PUBLICATIONS))";
         String expected1 = "SELECT DISTINCT `publication`.`title`, `publication`.`year`, COUNT(*) as `count` FROM `publication` JOIN `publication_references` ON `publication_references`.`pub2_id` = `publication`.`dblpKey` GROUP BY `publication`.`title` ORDER BY COUNT(*) DESC LIMIT 5;";
-        String expected2 = "SELECT DISTINCT `person`.`primaryName`, `person`.`orcid` FROM `person` JOIN `person_authored_publication` ON `person_authored_publication`.`personKey` = `person`.`dblpKey` JOIN (SELECT DISTINCT `publication`.`dblpKey` FROM `publication` JOIN `publication_references` ON `publication_references`.`pub2_id` = `publication`.`dblpKey` GROUP BY `publication`.`title` ORDER BY COUNT(*) DESC LIMIT 5) as publication_sub ON publication_sub.`dblpKey` = `person_authored_publication`.`publicationKey` LIMIT 100;";
+        String expected2 = "SELECT DISTINCT `person`.`primaryName`, `person`.`orcid` FROM `person` JOIN `person_authored_publication` ON `person_authored_publication`.`personKey` = `person`.`dblpKey` JOIN (SELECT DISTINCT `publication`.`dblpKey` FROM `publication` JOIN `publication_references` ON `publication_references`.`pub2_id` = `publication`.`dblpKey` GROUP BY `publication`.`title` ORDER BY COUNT(*) DESC LIMIT 5) as publication_authored_sub ON publication_authored_sub.`dblpKey` = `person_authored_publication`.`publicationKey` LIMIT 100;";
         try {
             assertEquals(expected1, Schenql.compileSchenQL(schenQLQuery1));
             assertEquals(expected2, Schenql.compileSchenQL(schenQLQuery2));
